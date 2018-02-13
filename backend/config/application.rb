dae02d594c3a16b3c379eb55d8549ffa88088a15
Tinusw <tinus@3months.com>
2018-02-13 23:03:15 +0100

    add axios for AJAX and cors middleware to enable AJAX requests

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,5 +29,16 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Todo
+    # Set this per environment instead of globally
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
+
+
--- frontend/src/components/ClientsContainer.js
+++ frontend/src/components/ClientsContainer.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ClientsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clients: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/clients.json')
+    .then(response => {
+      console.log(response)
+      this.setState({clients: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <h1>My Clients</h1>
+      <div>
+        {this.state.clients.map((client) => {
+          return(
+            <div className="tile" key={client.id} >
+              <h4>{client.first_name}</h4>
+              <p>{client.email}</p>
+            </div>
+          )
+        })}
+      </div>
     );
   }
 }
 
-export default ClientsContainer
+export default ClientsContainer;

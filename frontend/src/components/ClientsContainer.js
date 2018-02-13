2ac6e0d6bc1050b9e1202f8ce22da1b030fa43ff
Tinusw <tinus@3months.com>
2018-02-13 22:54:02 +0100

    creating a clients component

---
+++ frontend/src/components/ClientsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ClientsContainer extends Component {
+  render() {
+    return (
+      <h1>My Clients</h1>
+    );
+  }
+}
+
+export default ClientsContainer
